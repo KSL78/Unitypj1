cb2172f981d7c0234f3b606949d496e036c9920a
KSL78 <acc8041@gmail.com>
2024-04-22 01:52:17 +0900

    Create web.php

---
+++ test2/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/users/{name?}', function ($name = NULL) {
+//     return 'welcome, '. $name;
+// })->where('name','[a-zA-Z0-9]+');
+
+// Route::get('/product/{id?}', function ($id = NULL) {
+//     return '제품 id:'. $id;
+// })->where('id','[0-9]+');
+
+Route::get('/home/{name?}', [HomeController::class,'index'])->name('home.index');
+//Route::get('/{name}', [HomeController::class, 'index'])->name('home.index');
+// laravel 8 이상부터 namespace대신 use씀
